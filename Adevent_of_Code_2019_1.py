4f0d71cee20a51a4e881e2060be50050aeefebe3
Andrew <andrewwoodcock03@googlemail.com>
2020-12-14 18:48:35 +0000

    AoC 2019 1 part 1 & 2

--- Adevent_of_Code_2019_1.py
+++ Adevent_of_Code_2019_1.py
@@ -12,10 +12,22 @@ def get_req_fuel(module: int) -> int:
     return int(module / 3) - 2
 
 
+def get_req_fuel_recur(mass: int, fuel_sum=0) -> int:
+    fuel = int(mass / 3) - 2
+    print(fuel)
+    if fuel <= 0:
+        return fuel_sum
+    else:
+        fuel_sum += fuel
+        return get_req_fuel_recur(fuel, fuel_sum)
+
+
 def main():
     module_mass_list = get_file_data(module_masses)
     fuel_list = map(get_req_fuel, module_mass_list)
     print(sum(fuel_list))
+    re_fuel_list = map(get_req_fuel_recur, module_mass_list)
+    print(sum(re_fuel_list))
 
 
 if __name__ == '__main__':
